f9477f78d0d38efbdd6a366a685b2faba11ef2dd
Gabriel Royce <geroyceus@gmail.com>
2022-10-24 15:42:39 -0400

    weather app changes/improvements

--- Projects/WeatherApp/README.md
+++ Projects/WeatherApp/README.md
@@ -15,11 +15,17 @@
 | #     | Date       |
 | ----- | ---------- |
 | 1.0.0 | 10/19/2022 |
+| 1.1.0 | 10/24/2022 |
 
 ---
 
 ## Changes:
 
 **v1.0.0** - Initalized
+**v1.1.0** - Added Data Results section/functions. Page is functional
 
 ## Intended Changes
+
+> Modify CSS to be more presentable
+> Add specific dates function/5 day forecast
+> Add animations/images of weather
--- Projects/WeatherApp/script.js
+++ Projects/WeatherApp/script.js
@@ -4,12 +4,29 @@ const $locationSubmit = $('#locationSubmit');
 let tempsToday = {};
 let weatherInWords = {};
 let city = '';
+const weatherData = {};
+const temps = {};
+//Temp Conversion
 function tempConvert(type, temp) {
 	if (type == 'celsius') {
+		return Math.floor(temp - 273.15);
 	}
 	if (type == 'f') {
+		return Math.floor(((temp - 273.15) * 9) / 5 + 32);
 	}
 }
+
+//Data conversion
+function dataConvert(type, temps) {
+	for (const [key, value] of Object.entries(temps)) {
+		if (key != 'humidity' && key != 'pressure') {
+			temps[key] = tempConvert(type, value);
+		}
+	}
+	console.log(temps);
+}
+
+//Get and organize data
 
 $form.submit((element) => {
 	element.preventDefault();
@@ -23,6 +40,24 @@ $form.submit((element) => {
 		const apiCallWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2428f89e4496196c553a5b61abe465bf`;
 		$.ajax(apiCallWeather).then((element) => {
 			console.log(element);
+			weatherData['name'] = element.name;
+			weatherData['temps'] = element.main;
+			weatherData['feel'] = element.weather[0].description;
+			console.log(weatherData);
+			dataConvert('f', weatherData.temps);
+			renderData();
 		});
 	});
 });
+
+function renderData() {
+	const $weatherInfoToday = $('#weatherInfoToday');
+	$weatherInfoToday.html(`
+	<h1>${weatherData.name}</h1>
+	<div>
+		<p>Feels Like ${weatherData.feel}</p>
+		<p>Todays temperature is ${weatherData.temps.temp} with a high of ${weatherData.temps.temp_max} and a low of ${weatherData.temps.temp_min}</p>	
+	</div>
+	
+	`);
+}
